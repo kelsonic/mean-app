031780c100fcdad97ba47cc13ff4f09161bb883c
kelsonic <kgadams93@gmail.com>
2016-06-16 16:22:24 -0700

    Add message component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,10 +1,44 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: `  
-        <h1>Hello World!</h1>
-    `
+    template: `
+        <div class="row">
+            <section class="col-md-8 col-md-offset-2">  
+                <article class="panel panel-default">
+                    <div class="panel-body">
+                        {{ message.content }}
+                    </div>
+                    <footer>
+                        <div class="author">
+                            {{ message.author }}
+                        </div>
+                        <div class="config">
+                            <a href="#">Edit</a>
+                            <a href="#">Delete</a>
+                        </div>
+                    </footer>
+                </article>
+            </section>
+        </div>
+    `,
+    styles: [`
+        .author {
+            display: inline-block;
+            font-style: italic;
+            font-size: 12px;
+            width: 80%;
+        }
+        .config {
+            display: inline-block;
+            text-align: right;
+            font-size: 12px;
+            width: 19%;
+        }
+    `]
 })
 export class AppComponent {
-
+    message = {
+        content: 'A message',
+        author: 'Max'
+    };
 }
