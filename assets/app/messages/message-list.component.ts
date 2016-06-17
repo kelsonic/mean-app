980295e4d41d3b0a0ba221aa2502a22650d95000
kelsonic <kgadams93@gmail.com>
2016-06-17 10:07:41 -0700

    Add message input component to my-app

---
+++ assets/app/messages/message-list.component.ts
@@ -0,0 +1,20 @@
+import {Component} from 'angular2/core';
+import {MessageComponent} from './message.component';
+import {Message} from './message';
+@Component({
+    selector: 'my-message-list',
+    template: `
+        <section class="col-md-8 col-md-offset-2">  
+            <my-message *ngFor="#message of messages" [message]="message" (editClicked)="message.content = $event"></my-message>
+        </section>
+    `,
+    directives: [MessageComponent]
+})
+
+
+export class MessageListComponent {
+    messages: Message[] = [
+        new Message('A new message', null, 'Max'),
+        new Message('Another message', null, 'Anna')
+    ];
+}
