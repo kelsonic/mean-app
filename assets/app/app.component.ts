5c333e3d8bf77daa760465c31c11b1ee91695337
kelsonic <kgadams93@gmail.com>
2016-06-16 20:31:27 -0700

    Add attribute directives

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,24 +1,19 @@
 import {Component} from 'angular2/core';
 import {MessageComponent} from './messages/message.component';
+import {Message} from './messages/message';
 @Component({
     selector: 'my-app',
     template: `
         <div class="row">
-            <section class="col-md-8 col-md-offset-2">
-                <input type="text" [(ngModel)]="message.content">
-            </section>
         </div>
         <div class="row">
             <section class="col-md-8 col-md-offset-2">  
-                <my-message></my-message>
+                <my-message [message]="message" (editClicked)="message.content = $event"></my-message>
             </section>
         </div>
     `,
     directives: [MessageComponent]
 })
 export class AppComponent {
-    message = {
-        content: 'A message',
-        author: 'Max'
-    };
+    message: Message = new Message('A new message', null, 'Max');
 }
