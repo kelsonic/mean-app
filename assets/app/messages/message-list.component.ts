3be54dda679bda3f7aff1b8243e5fbf91a8c159a
kelsonic <kgadams93@gmail.com>
2016-06-18 17:11:20 -0700

    Update message list to get messages on init

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -20,6 +20,12 @@ export class MessageListComponent implements OnInit {
     messages: Message[];
 
     ngOnInit() {
-        this.messages = this._messageService.getMessages();
+        this._messageService.getMessages()
+            .subscribe(
+                messages => {
+                    this.messages = messages;
+                    this._messageService.messages = messages;
+                }
+            );
     }
 }
