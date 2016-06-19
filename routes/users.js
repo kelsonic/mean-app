fa96d28a8e382d0178b66a94ae748c5eea7ccaf7
kelsonic <kgadams93@gmail.com>
2016-06-18 20:05:01 -0700

    Add jsonwebtoken npm, verify password, and assign token with 1 hour expiration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passwordHash = require('password-hash');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -23,6 +24,35 @@ router.post('/', function(req, res, next) {
       obj: result
     });
   })
+});
+
+router.post('/login', function(req, res, next) {
+  User.findOne({email: req.body.email}, function(err, doc) {
+    if (err) {
+      return res.status(404).json({
+        title: 'An error occured',
+        error: err
+      });
+    }
+    if (!doc) {
+      return res.status(404).json({
+        title: 'No user found',
+        error: {message: 'User not found'}
+      });
+    }
+    if (!passwordHash.verify(req.body.password, doc.password)) {
+      return res.status(404).json({
+        title: 'Could not log you in.',
+        error: {message: 'Invalid username or password.'}
+      });
+    }
+    // Authentication expires in 1 hour
+    var token = jwt.sign({user: doc}, 'jquerty!@#$', {expiresIn: 3600});
+    res.status(200).json({
+      message: 'Success',
+      obj: token
+    });
+  });
 });
 
 module.exports = router;
