c42c846cb61c53efd309f888b08fa6569476afbc
kelsonic <kgadams93@gmail.com>
2016-06-18 19:51:43 -0700

    Add signup component using auth service

--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from 'angular2/core';
 import {FormBuilder, ControlGroup, Validators, Control} from 'angular2/common';
+import {User} from './user';
+import {AuthService} from './auth.service';
 
 // I used a data-driven approach as opposed to template-driven
 @Component({
@@ -33,10 +35,15 @@ export class SignupComponent implements OnInit {
 
     myForm: ControlGroup;
 
-    constructor(private _fb:FormBuilder) {}
+    constructor(private _fb:FormBuilder, private _authService: AuthService) {}
 
     onSubmit() {
-        console.log(this.myForm.value);
+        const user = new User(this.myForm.value.email, this.myForm.value.password, this.myForm.value.firstName, this.myForm.value.lastName);
+        this._authService.signup(user)
+            .subscribe(
+                data => console.log(data),
+                error => console.error(error)
+            )
     }
 
     ngOnInit() {
