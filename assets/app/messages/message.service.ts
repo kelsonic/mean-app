ece807015ec01f5bdf4f921a0b0cd4447ef867af
kelsonic <kgadams93@gmail.com>
2016-06-18 18:10:38 -0700

    Return delete observable

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -52,5 +52,8 @@ export class MessageService {
 
     deleteMessage(message: Message) {
         this.messages.splice(this.messages.indexOf(message), 1);
+        return this._http.delete('http://localhost:3000/message/' + message.messageId)
+            .map(response => response.json())
+            .catch(error => Observable.throw(error.json()));
     }
 }
