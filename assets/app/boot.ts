89b55f1718caa13ae7821c1782efdb81a2e4b577
kelsonic <kgadams93@gmail.com>
2016-06-18 19:52:50 -0700

    Add auth service

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -2,8 +2,9 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 import {MessageService} from './messages/message.service';
+import {AuthService} from './auth/auth.service';
 import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {provide} from 'angular2/core';
 import {HTTP_PROVIDERS} from 'angular2/http';
 
-bootstrap(AppComponent, [MessageService, ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy}), HTTP_PROVIDERS]);
+bootstrap(AppComponent, [MessageService, AuthService, ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy}), HTTP_PROVIDERS]);
