0d9b76cd6a8c11d441eebb9c5f7e5635c736284b
kelsonic <kgadams93@gmail.com>
2016-06-18 17:12:12 -0700

    Add response to observable

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -15,12 +15,26 @@ export class MessageService {
         const headers = new Headers({'Content-Type': 'application/json'});
         // Return an observable
         return this._http.post('http://localhost:3000/message', body, {headers: headers})
-            .map(response => response.json())
+            .map(response => {
+                const data = response.json().obj;
+                let message = new Message(data.content, data._id, 'Dummy', null);
+                return message;
+            })
             .catch(error => Observable.throw(error.json()));
     }
 
     getMessages() {
-        return this.messages;
+        return this._http.get('http://localhost:3000/message')
+            .map(response => {
+                const data = response.json().obj;
+                let objs: any[] = [];
+                for (let i = 0; i < data.length; i++) {
+                    let message = new Message(data[i].content, data[i]._id, 'Dummy', null);
+                    objs.push(message);
+                };
+                return objs;
+            })
+            .catch(error => Observable.throw(error.json()));
     }
 
     editMessage(message: Message) {
