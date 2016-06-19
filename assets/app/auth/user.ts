c3d74cd43ad8adb16c3f0620c7b1ba20c43cbb51
kelsonic <kgadams93@gmail.com>
2016-06-18 19:52:09 -0700

    Add public to all user properties

--- assets/app/auth/user.ts
+++ assets/app/auth/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    constructor(public email: string, password: string, firstName?: string, lastName?: string) {
+    constructor(public email: string, public password: string, public firstName?: string, public lastName?: string) {
         
     }
 }
